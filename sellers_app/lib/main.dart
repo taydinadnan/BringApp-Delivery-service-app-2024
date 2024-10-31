8e807e74eeecbd5d31f9cef738c1ff13e010d6ac
taydinadnan <taydinadnan@gmail.com>
2024-10-31 15:53:53 +0100

    sellers_app splash screen

--- sellers_app/lib/main.dart
+++ sellers_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sellers_app/splashScreen/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: const MySplashScreen(),
     );
   }
 }
