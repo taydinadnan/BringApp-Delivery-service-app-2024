9f33c3d49bf4999e252aefb1ee2fe11124c3979a
taydinadnan <taydinadnan@gmail.com>
2024-11-01 11:21:39 +0100

    Sign Up form

--- sellers_app/lib/authentication/register.dart
+++ sellers_app/lib/authentication/register.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sellers_app/widgets/custom_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -9,16 +12,121 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-  TextEditingController anyController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomTextField(
-        controller: anyController,
-        data: Icons.phone,
-        hintText: "Phone",
-        isObscure: false,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          const SizedBox(height: 10),
+          InkWell(
+            child: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.20,
+              backgroundColor: Colors.white,
+              backgroundImage: imageXFile == null
+                  ? null
+                  : FileImage(
+                      File(imageXFile!.path),
+                    ),
+              child: imageXFile == null
+                  ? Icon(
+                      Icons.add_photo_alternate,
+                      size: MediaQuery.of(context).size.width * 0.20,
+                      color: Colors.grey,
+                    )
+                  : null,
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  data: Icons.person,
+                  controller: nameController,
+                  hintText: "name",
+                  isObscure: false,
+                ),
+                CustomTextField(
+                  data: Icons.email,
+                  controller: emailController,
+                  hintText: "Email",
+                  isObscure: false,
+                ),
+                CustomTextField(
+                  data: Icons.password,
+                  controller: passwordController,
+                  hintText: "Password",
+                  isObscure: true,
+                ),
+                CustomTextField(
+                  data: Icons.password,
+                  controller: confirmController,
+                  hintText: "Confirm Password",
+                  isObscure: true,
+                ),
+                CustomTextField(
+                  data: Icons.phone,
+                  controller: phoneController,
+                  hintText: "Phone",
+                  isObscure: false,
+                ),
+                CustomTextField(
+                  data: Icons.my_location,
+                  controller: locationController,
+                  hintText: "Cafe/Restaurant Address",
+                  isObscure: false,
+                  enabled: false,
+                ),
+                Container(
+                  width: 400,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: ElevatedButton.icon(
+                    label: const Text(
+                      "Get My Current Location",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    icon: const Icon(
+                      Icons.location_on,
+                      color: Colors.white,
+                    ),
+                    onPressed: () => print("Clicked"),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        )),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () => print("Clicked"),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                )),
+            child: const Text(
+              "Sign Up",
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
       ),
     );
   }
